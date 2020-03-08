f68c3f3cd9951733a8e1f7c3557cdb1821ee7f95
YR sun <717437709@qq.com>
2020-03-08 10:01:23 +0800

    细节的优化

--- build/app.php
+++ build/app.php
@@ -1,18 +1,19 @@
 <?php
-/**
- * 首先引入Composer相关依赖
- */
+
+// 首先引入Composer相关依赖
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 // 读取配置文件
 (new Canon\Framework\Boot\Configuration())->load();
 
 // 配置容器
-$app = new \Canon\Framework\Application();
+$app = new Canon\Framework\Application();
 
 // 路由配置
 $router = $app->router->CreateRouter();
 
 require_once __DIR__.'/../app/routes.php';
 
+// 返回容器
 return $app;
