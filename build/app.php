8f8f17c1b4c6369a015aa1235753c2c4f2ae943a
YR sun <717437709@qq.com>
2020-03-08 16:44:11 +0800

    优化

--- build/app.php
+++ build/app.php
@@ -1,7 +1,6 @@
 <?php
 
 // 首先引入Composer相关依赖
-
 require_once __DIR__.'/../vendor/autoload.php';
 
 // 读取配置文件
@@ -10,10 +9,15 @@ require_once __DIR__.'/../vendor/autoload.php';
 // 配置容器
 $app = new Canon\Framework\Application();
 
+// 注册Facades
+$app->withFacades();
+// 注册ORM
+$app->withEloquent();
+
 // 路由配置
 $router = $app->router->CreateRouter();
-
 require_once __DIR__.'/../app/routes.php';
 
 // 返回容器
 return $app;
+
