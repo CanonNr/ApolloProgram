247ad77e13eba56ae9dbbf0f24b0f5612f44a076
canon1014 <717437709@qq.com>
2020-03-19 22:19:40 +0800

    更新readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-## 暂时只存放log文件
+## Apollo Program
+
+短期开发计划:
+- 集成Swoole
+- RPC
+- 注解开发
+- 定时任务
+- ORM
+- log
+
+已完成:
+- ENV配置
+- 路由
--- build/app.php
+++ build/app.php
@@ -9,13 +9,9 @@ require_once __DIR__.'/../vendor/autoload.php';
 // 配置容器
 $app = new Canon\Framework\Application();
 
-// 注册Facades
-$app->withFacades();
-// 注册ORM
-$app->withEloquent();
-
 // 路由配置
 $router = $app->router->CreateRouter();
+
 require_once __DIR__.'/../app/routes.php';
 
 // 返回容器
