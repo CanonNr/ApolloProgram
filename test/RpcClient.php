03b732e7585be175bcd9e6897a046d677e3ecb61
canon1014 <717437709@qq.com>
2020-03-29 11:22:16 +0800

    rpc测试

--- test/RpcClient.php
+++ test/RpcClient.php
@@ -5,6 +5,9 @@ require_once '../vendor/autoload.php';
 
 
 
-$client = new Client(1,1);
-$client->class("test")->get(1);
+$client = new Client();
+$result = $client::getInstance()->set('127.0.0.1',9501)->class("test")->get(1);
+
+var_dump($result.'222');
+//$client::getInstance()->set(1,1)->class("test")->get(1);
 //var_dump($client);
